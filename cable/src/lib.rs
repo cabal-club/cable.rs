9dd33d68a306a8a4bc2fb1b428c3d8731db7b016
mycognosist <gnomad@cryptolab.net>
2023-07-06 17:27:38 +0200

    add todo note about chaning Channel and Text types from Vec<u8> to String

--- cable/src/lib.rs
+++ cable/src/lib.rs
@@ -2,11 +2,15 @@ mod error;
 mod message;
 mod post;
 
+// TODO: Consider changing this to `String`.
+// Then use `.into_bytes()` where required.
 pub type Channel = Vec<u8>;
 pub type CircuitId = [u8; 4];
 pub type Hash = [u8; 32];
 pub type ReqId = [u8; 4];
+// TODO: Consider changing this to `String`.
 pub type Text = Vec<u8>;
+// TODO: Consider changing this to `String`.
 pub type Topic = Vec<u8>;
 
 #[derive(Clone, Debug)]
