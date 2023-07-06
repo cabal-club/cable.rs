157afbe18d8816e7161a1f6968e9009b29338631
mycognosist <gnomad@cryptolab.net>
2023-07-06 08:48:07 +0200

    remove some commented-out code and add todo comments

--- cable/src/post.rs
+++ cable/src/post.rs
@@ -55,8 +55,7 @@ pub struct PostHeader {
     pub public_key: [u8; 32],
     /// Signature of the fields that follow.
     pub signature: [u8; 64],
-    // TODO: Consider removing this field.
-    // Can be inferred from `links`.
+    // TODO: Remove this field. Can be calculated from the length of `links`.
     /// Number of hashes this post links back to (0+).
     pub num_links: u64,
     /// Hashes of the latest posts in this channel/context.
@@ -102,10 +101,12 @@ impl PostHeader {
 pub enum PostBody {
     /// Post a chat message to a channel.
     Text {
+        // TODO: Remove this field. Can be calculated from the length of `channel`.
         /// Length of the channel's name in bytes.
         channel_len: ChannelLen,
         /// Channel name (UTF-8).
         channel: Channel,
+        // TODO: Remove this field. Can be calculated from the length of `text`.
         /// Length of the text field in bytes.
         text_len: u64,
         /// Chat message text (UTF-8).
@@ -450,7 +451,7 @@ mod test {
 
     #[test]
     fn text_post_to_bytes() {
-        // TODO: Return `Result` so and replace `unwrap()` with `?`.
+        // TODO: Return `Result` and replace `unwrap()` with `?`.
 
         // Field values sourced from https://github.com/cabal-club/cable.js#examples.
 
@@ -562,218 +563,6 @@ mod test {
 }
 
 /*
-impl Post {
-    pub fn post_type(&self) -> u64 {
-        match &self.body {
-            PostBody::Text { .. } => 0,
-            PostBody::Delete { .. } => 1,
-            PostBody::Info { .. } => 2,
-            PostBody::Topic { .. } => 3,
-            PostBody::Join { .. } => 4,
-            PostBody::Leave { .. } => 5,
-            PostBody::Unrecognized { post_type } => *post_type,
-        }
-    }
-    pub fn verify(buf: &[u8]) -> bool {
-        if buf.len() < 32 + 64 {
-            return false;
-        }
-        let o_pk = crypto::sign::PublicKey::from_slice(&buf[0..32]);
-        let o_sig = crypto::sign::Signature::from_bytes(&buf[32..32 + 64]);
-        match (o_pk, o_sig) {
-            (Some(pk), Ok(sig)) => crypto::sign::verify_detached(&sig, &buf[32 + 64..], &pk),
-            _ => false,
-        }
-    }
-    pub fn sign(&mut self, secret_key: &[u8; 64]) -> Result<(), Error> {
-        let buf = self.to_bytes()?;
-        let sk = crypto::sign::SecretKey::from_slice(secret_key).unwrap();
-        // todo: return NoneError
-        self.header.signature = crypto::sign::sign_detached(&buf[32 + 64..], &sk).to_bytes();
-        Ok(())
-    }
-    pub fn is_signed(&self) -> bool {
-        for i in 0..self.header.signature.len() {
-            if self.header.signature[i] != 0 {
-                return true;
-            }
-        }
-        return false;
-    }
-    pub fn hash(&self) -> Result<Hash, Error> {
-        let buf = self.to_bytes()?;
-        let digest = crypto::generichash::hash(&buf, Some(32), None).unwrap();
-        Ok(digest.as_ref().try_into()?)
-    }
-    pub fn get_timestamp(&self) -> Option<u64> {
-        match &self.body {
-            PostBody::Text { timestamp, .. } => Some(*timestamp),
-            PostBody::Delete { timestamp, .. } => Some(*timestamp),
-            PostBody::Info { timestamp, .. } => Some(*timestamp),
-            PostBody::Topic { timestamp, .. } => Some(*timestamp),
-            PostBody::Join { timestamp, .. } => Some(*timestamp),
-            PostBody::Leave { timestamp, .. } => Some(*timestamp),
-            PostBody::Unrecognized { .. } => None,
-        }
-    }
-    pub fn get_channel<'a>(&'a self) -> Option<&'a Channel> {
-        match &self.body {
-            PostBody::Text { channel, .. } => Some(channel),
-            PostBody::Delete { .. } => None,
-            PostBody::Info { .. } => None,
-            PostBody::Topic { channel, .. } => Some(channel),
-            PostBody::Join { channel, .. } => Some(channel),
-            PostBody::Leave { channel, .. } => Some(channel),
-            PostBody::Unrecognized { .. } => None,
-        }
-    }
-}
-
-impl CountBytes for Post {
-    fn count_bytes(&self) -> usize {
-        let post_type = self.post_type();
-        let header_size = 32 + 64 + 32;
-        let body_size = varint::length(post_type)
-            + match &self.body {
-                PostBody::Text {
-                    channel,
-                    timestamp,
-                    text,
-                } => {
-                    varint::length(channel.len() as u64)
-                        + channel.len()
-                        + varint::length(*timestamp)
-                        + varint::length(text.len() as u64)
-                        + text.len()
-                }
-                PostBody::Delete { timestamp, hash } => varint::length(*timestamp) + hash.len(),
-                PostBody::Info {
-                    timestamp,
-                    key,
-                    value,
-                } => {
-                    varint::length(*timestamp)
-                        + varint::length(key.len() as u64)
-                        + key.len()
-                        + varint::length(value.len() as u64)
-                        + value.len()
-                }
-                PostBody::Topic {
-                    channel,
-                    timestamp,
-                    topic,
-                } => {
-                    varint::length(channel.len() as u64)
-                        + channel.len()
-                        + varint::length(*timestamp)
-                        + varint::length(topic.len() as u64)
-                        + topic.len()
-                }
-                PostBody::Join { channel, timestamp } => {
-                    varint::length(channel.len() as u64)
-                        + channel.len()
-                        + varint::length(*timestamp)
-                }
-                PostBody::Leave { channel, timestamp } => {
-                    varint::length(channel.len() as u64)
-                        + channel.len()
-                        + varint::length(*timestamp)
-                }
-                PostBody::Unrecognized { .. } => 0,
-            };
-        header_size + body_size
-    }
-    fn count_from_bytes(_buf: &[u8]) -> Result<usize, Error> {
-        unimplemented![]
-    }
-}
-
-impl ToBytes for Post {
-    fn to_bytes(&self) -> Result<Vec<u8>, Error> {
-        let mut buf = vec![0; self.count_bytes()];
-        self.write_bytes(&mut buf)?;
-        Ok(buf)
-    }
-    fn write_bytes(&self, buf: &mut [u8]) -> Result<usize, Error> {
-        let mut offset = 0;
-        assert_eq![self.header.public_key.len(), 32];
-        assert_eq![self.header.signature.len(), 64];
-        assert_eq![self.header.link.len(), 32];
-        buf[offset..offset + 32].copy_from_slice(&self.header.public_key);
-        offset += self.header.public_key.len();
-        buf[offset..offset + 64].copy_from_slice(&self.header.signature);
-        offset += self.header.signature.len();
-        buf[offset..offset + 32].copy_from_slice(&self.header.link);
-        offset += self.header.link.len();
-        offset += varint::encode(self.post_type(), &mut buf[offset..])?;
-        match &self.body {
-            PostBody::Text {
-                channel,
-                timestamp,
-                text,
-            } => {
-                offset += varint::encode(channel.len() as u64, &mut buf[offset..])?;
-                buf[offset..offset + channel.len()].copy_from_slice(channel);
-                offset += channel.len();
-                offset += varint::encode(*timestamp, &mut buf[offset..])?;
-                offset += varint::encode(text.len() as u64, &mut buf[offset..])?;
-                buf[offset..offset + text.len()].copy_from_slice(text);
-                offset += text.len();
-            }
-            PostBody::Delete { timestamp, hash } => {
-                offset += varint::encode(*timestamp, &mut buf[offset..])?;
-                buf[offset..offset + hash.len()].copy_from_slice(hash);
-                offset += hash.len();
-            }
-            PostBody::Info {
-                timestamp,
-                key,
-                value,
-            } => {
-                offset += varint::encode(*timestamp, &mut buf[offset..])?;
-                offset += varint::encode(key.len() as u64, &mut buf[offset..])?;
-                buf[offset..offset + key.len()].copy_from_slice(key);
-                offset += key.len();
-                offset += varint::encode(value.len() as u64, &mut buf[offset..])?;
-                buf[offset..offset + value.len()].copy_from_slice(value);
-                offset += value.len();
-            }
-            PostBody::Topic {
-                channel,
-                timestamp,
-                topic,
-            } => {
-                offset += varint::encode(channel.len() as u64, &mut buf[offset..])?;
-                buf[offset..offset + channel.len()].copy_from_slice(channel);
-                offset += channel.len();
-                offset += varint::encode(*timestamp, &mut buf[offset..])?;
-                offset += varint::encode(topic.len() as u64, &mut buf[offset..])?;
-                buf[offset..offset + topic.len()].copy_from_slice(topic);
-                offset += topic.len();
-            }
-            PostBody::Join { channel, timestamp } => {
-                offset += varint::encode(channel.len() as u64, &mut buf[offset..])?;
-                buf[offset..offset + channel.len()].copy_from_slice(channel);
-                offset += channel.len();
-                offset += varint::encode(*timestamp, &mut buf[offset..])?;
-            }
-            PostBody::Leave { channel, timestamp } => {
-                offset += varint::encode(channel.len() as u64, &mut buf[offset..])?;
-                buf[offset..offset + channel.len()].copy_from_slice(channel);
-                offset += channel.len();
-                offset += varint::encode(*timestamp, &mut buf[offset..])?;
-            }
-            PostBody::Unrecognized { post_type } => {
-                return E::PostWriteUnrecognizedType {
-                    post_type: *post_type,
-                }
-                .raise();
-            }
-        }
-        Ok(offset)
-    }
-}
-
 impl FromBytes for Post {
     fn from_bytes(buf: &[u8]) -> Result<(usize, Self), Error> {
         let mut offset = 0;
