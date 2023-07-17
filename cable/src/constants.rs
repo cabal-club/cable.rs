eacca99880d661ca574127d4ef089f7463076e33
mycognosist <gnomad@cryptolab.net>
2023-07-17 10:50:39 +0200

    add message and post constants to file

---
+++ cable/src/constants.rs
@@ -0,0 +1,22 @@
+/* POST FIELD VALUES */
+
+pub const TEXT_POST: u64 = 0;
+pub const DELETE_POST: u64 = 1;
+pub const INFO_POST: u64 = 2;
+pub const TOPIC_POST: u64 = 3;
+pub const JOIN_POST: u64 = 4;
+pub const LEAVE_POST: u64 = 5;
+
+/* RESPONSE FIELD VALUES */
+
+pub const HASH_RESPONSE: u64 = 0;
+pub const POST_RESPONSE: u64 = 1;
+pub const CHANNEL_LIST_RESPONSE: u64 = 7;
+
+/* REQUEST FIELD VALUES */
+
+pub const POST_REQUEST: u64 = 2;
+pub const CANCEL_REQUEST: u64 = 3;
+pub const CHANNEL_TIME_RANGE_REQUEST: u64 = 4;
+pub const CHANNEL_STATE_REQUEST: u64 = 5;
+pub const CHANNEL_LIST_REQUEST: u64 = 6;
