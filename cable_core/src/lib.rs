19ce9836f24f371e23d67efba257cfbb49cda7cb
mycognosist <gnomad@cryptolab.net>
2023-07-18 14:35:23 +0200

    remove redundant ChannelOptions struct

--- cable_core/src/lib.rs
+++ cable_core/src/lib.rs
@@ -4,13 +4,3 @@
 mod manager;
 mod store;
 mod stream;
-
-use cable::{Channel, Timestamp};
-
-#[derive(Clone, Debug, PartialEq)]
-pub struct ChannelOptions {
-    pub channel: Channel,
-    pub time_start: Timestamp,
-    pub time_end: Timestamp,
-    pub limit: usize,
-}
