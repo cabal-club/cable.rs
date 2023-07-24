8342905f6de3a7c9b71ca682813e4b365b514f81
mycognosist <gnomad@cryptolab.net>
2023-07-24 14:47:46 +0200

    implement Display for Message

--- cable/src/message.rs
+++ cable/src/message.rs
@@ -9,6 +9,7 @@
 use std::fmt;
 
 use desert::{varint, CountBytes, FromBytes, ToBytes};
+use sodiumoxide::hex;
 
 use crate::{
     constants::{
@@ -170,10 +171,10 @@ impl Message {
     }
 }
 
+/// Print a message with byte arrays formatted as hex strings.
 impl fmt::Display for Message {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // TODO
-        write!(f, "")
+        write!(f, "{{ {}, {} }}", &self.header, &self.body)
     }
 }
 
@@ -199,6 +200,20 @@ impl MessageHeader {
     }
 }
 
+/// Print a message header with byte arrays formatted as hex strings.
+impl fmt::Display for MessageHeader {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let circuit_id_hex = hex::encode(&self.circuit_id);
+        let req_id_hex = hex::encode(&self.req_id);
+
+        write!(
+            f,
+            "msg_type: {}, circuit_id: {:?}, req_id: {:?}",
+            &self.msg_type, circuit_id_hex, req_id_hex
+        )
+    }
+}
+
 #[derive(Clone, Debug)]
 /// The body of a request or response message.
 pub enum MessageBody {
@@ -214,6 +229,23 @@ pub enum MessageBody {
     Unrecognized {
         msg_type: u64,
     },
+}
+
+/// Print a message body with byte arrays formatted as hex strings.
+impl fmt::Display for MessageBody {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            MessageBody::Request { ttl, body } => {
+                write!(f, "ttl: {}, {}", ttl, body)
+            }
+            MessageBody::Response { body } => {
+                write!(f, "{}", body)
+            }
+            MessageBody::Unrecognized { msg_type: _ } => {
+                write!(f, "msg_type: unrecognized")
+            }
+        }
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -297,6 +329,40 @@ pub enum RequestBody {
     },
 }
 
+/// Print a message request body with byte arrays formatted as hex strings.
+impl fmt::Display for RequestBody {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            RequestBody::Post { hashes } => {
+                let hashes_hex: Vec<String> = hashes.iter().map(|hash| hex::encode(hash)).collect();
+                write!(f, "hashes: {:?}", hashes_hex)
+            }
+            RequestBody::Cancel { cancel_id } => {
+                let cancel_id_hex = hex::encode(cancel_id);
+                write!(f, "cancel_id: {:?}", cancel_id_hex)
+            }
+            RequestBody::ChannelTimeRange {
+                channel,
+                time_start,
+                time_end,
+                limit,
+            } => {
+                write!(
+                    f,
+                    "channel: {:?}, time_start: {}, time_end: {}, limit: {}",
+                    channel, time_start, time_end, limit
+                )
+            }
+            RequestBody::ChannelState { channel, future } => {
+                write!(f, "channel: {:?}, future: {}", channel, future)
+            }
+            RequestBody::ChannelList { skip, limit } => {
+                write!(f, "offset: {}, limit: {}", skip, limit)
+            }
+        }
+    }
+}
+
 #[derive(Clone, Debug)]
 /// The body of a response message.
 pub enum ResponseBody {
@@ -312,7 +378,6 @@ pub enum ResponseBody {
     /// Message type (`msg_type`) is `1`.
     Post {
         /// A list of encoded posts, with each one including the length and data of the post.
-        // TODO: Should this be `Post` instead of `Payload`?
         posts: Vec<Payload>,
     },
     /// Respond with a list of names of known channels.
@@ -322,6 +387,25 @@ pub enum ResponseBody {
         /// A list of channels, with each one including the length and name of a channel.
         channels: Vec<Channel>,
     },
+}
+
+/// Print a message response body with byte arrays formatted as hex strings.
+impl fmt::Display for ResponseBody {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ResponseBody::Hash { hashes } => {
+                let hashes_hex: Vec<String> = hashes.iter().map(|hash| hex::encode(hash)).collect();
+                write!(f, "hashes: {:?}", hashes_hex)
+            }
+            ResponseBody::Post { posts } => {
+                let posts_hex: Vec<String> = posts.iter().map(|post| hex::encode(post)).collect();
+                write!(f, "posts: {:?}", posts_hex)
+            }
+            ResponseBody::ChannelList { channels } => {
+                write!(f, "channels: {:?}", channels)
+            }
+        }
+    }
 }
 
 impl CountBytes for Message {
@@ -869,6 +953,7 @@ mod test {
 
     #[test]
     fn post_request_to_bytes() -> Result<(), Error> {
+        // TODO: Use `sodiumoxide::hex::decode` instead.
         let req_id = <[u8; 4]>::from_hex(REQ_ID)?;
         let hashes: Vec<Hash> = vec![
             <[u8; 32]>::from_hex(HASH_1)?,
