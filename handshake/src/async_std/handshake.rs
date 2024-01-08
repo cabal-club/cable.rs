4a31ddc93bf48f11481940f2571691a50cf3b6d7
mycognosist <gnomad@cryptolab.net>
2024-01-08 15:31:44 +0200

    add async handshake wrappers for client and server

--- handshake/src/async_std/handshake.rs
+++ handshake/src/async_std/handshake.rs
@@ -1,12 +1,12 @@
-use std::io::{Read, Write};
+use futures_util::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 
 use crate::{
     ephemeral_and_static_key_bytes_len, ephemeral_key_bytes_len, static_key_bytes_len,
     version_bytes_len, Handshake, HandshakeComplete, Result, Version,
 };
 
-/// Initiate the handshake over a synchronous stream and run to completion.
-pub fn client<T: Read + Write>(
+/// Initiate the handshake over an asynchronous stream and run to completion.
+pub async fn client<T: AsyncRead + AsyncWrite + Unpin>(
     stream: &mut T,
     version: Version,
     psk: [u8; 32],
@@ -19,11 +19,11 @@ pub fn client<T: Read + Write>(
     // Send version.
     let send_buf = &mut buf[..version_bytes_len()];
     let handshake = handshake.send_client_version(send_buf)?;
-    stream.write_all(send_buf)?;
+    stream.write_all(send_buf).await?;
 
     // Receive version.
     let recv_buf = &mut buf[..version_bytes_len()];
-    stream.read_exact(recv_buf)?;
+    stream.read_exact(recv_buf).await?;
     let handshake = handshake.recv_server_version(recv_buf)?;
 
     // Build Noise state machine.
@@ -32,25 +32,25 @@ pub fn client<T: Read + Write>(
     // Send ephemeral key.
     let send_buf = &mut buf[..ephemeral_key_bytes_len()];
     let handshake = handshake.send_client_ephemeral_key(send_buf)?;
-    stream.write_all(send_buf)?;
+    stream.write_all(send_buf).await?;
 
     // Receive ephemeral and static keys.
     let recv_buf = &mut buf[..ephemeral_and_static_key_bytes_len()];
-    stream.read_exact(recv_buf)?;
+    stream.read_exact(recv_buf).await?;
     let handshake = handshake.recv_server_ephemeral_and_static_key(recv_buf)?;
 
     // Send static key.
     let send_buf = &mut buf[..static_key_bytes_len()];
     let handshake = handshake.send_client_static_key(send_buf)?;
-    stream.write_all(send_buf)?;
+    stream.write_all(send_buf).await?;
 
     let handshake = handshake.init_client_transport_mode()?;
 
     Ok(handshake)
 }
 
-/// Respond to a handshake over a synchronous stream and run to completion.
-pub fn server<T: Read + Write>(
+/// Respond to a handshake over an asynchronous stream and run to completion.
+pub async fn server<T: AsyncRead + AsyncWrite + Unpin>(
     stream: &mut T,
     version: Version,
     psk: [u8; 32],
@@ -62,30 +62,30 @@ pub fn server<T: Read + Write>(
 
     // Receive version.
     let recv_buf = &mut buf[..version_bytes_len()];
-    stream.read_exact(recv_buf)?;
+    stream.read_exact(recv_buf).await?;
     let handshake = handshake.recv_client_version(recv_buf)?;
 
     // Send version.
     let send_buf = &mut buf[..version_bytes_len()];
     let handshake = handshake.send_server_version(send_buf)?;
-    stream.write_all(send_buf)?;
+    stream.write_all(send_buf).await?;
 
     // Build Noise state machine.
     let handshake = handshake.build_server_noise_state_machine()?;
 
     // Receive ephemeral key.
     let recv_buf = &mut buf[..ephemeral_key_bytes_len()];
-    stream.read_exact(recv_buf)?;
+    stream.read_exact(recv_buf).await?;
     let handshake = handshake.recv_client_ephemeral_key(recv_buf)?;
 
     // Send ephemeral and static keys.
     let send_buf = &mut buf[..ephemeral_and_static_key_bytes_len()];
     let handshake = handshake.send_server_ephemeral_and_static_key(send_buf)?;
-    stream.write_all(send_buf)?;
+    stream.write_all(send_buf).await?;
 
     // Receive static key.
     let recv_buf = &mut buf[..static_key_bytes_len()];
-    stream.read_exact(recv_buf)?;
+    stream.read_exact(recv_buf).await?;
     let handshake = handshake.recv_client_static_key(recv_buf)?;
 
     let handshake = handshake.init_server_transport_mode()?;
