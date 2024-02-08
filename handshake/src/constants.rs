ef6310f31a4c86150dab65bace0d3b27d2920a97
mycognosist <gnomad@cryptolab.net>
2024-02-08 09:02:51 +0200

    move consts to separate module and update usage

---
+++ handshake/src/constants.rs
@@ -0,0 +1,19 @@
+//! Constant values used during handshake operations.
+
+/// Size of the ephemeral key.
+pub const EPHEMERAL_KEY_BYTES_LEN: usize = 48;
+
+/// Size of the ephemeral and static keys.
+pub const EPHEMERAL_AND_STATIC_KEY_BYTES_LEN: usize = 96;
+
+/// Size of the public key received via the static key exchange.
+pub const PUBLIC_KEY_BYTES_LEN: usize = 32;
+
+/// Size of the static key.
+///
+/// In the context of the Cable Handshake implementation, this value is the
+/// private key of the author keypair.
+pub const STATIC_KEY_BYTES_LEN: usize = 64;
+
+/// Size of the version message.
+pub const VERSION_BYTES_LEN: usize = 2;
--- handshake/src/sync/handshake.rs
+++ handshake/src/sync/handshake.rs
@@ -5,8 +5,11 @@
 use std::io::{Read, Write};
 
 use crate::{
-    ephemeral_and_static_key_bytes_len, ephemeral_key_bytes_len, static_key_bytes_len,
-    version_bytes_len, Handshake, HandshakeComplete, Result, Version,
+    constants::{
+        EPHEMERAL_AND_STATIC_KEY_BYTES_LEN, EPHEMERAL_KEY_BYTES_LEN, STATIC_KEY_BYTES_LEN,
+        VERSION_BYTES_LEN,
+    },
+    Handshake, HandshakeComplete, Result, Version,
 };
 
 /// Initiate the handshake over a synchronous stream and run to completion.
@@ -21,12 +24,12 @@ pub fn client<T: Read + Write>(
     let handshake = Handshake::new_client(version, psk, private_key);
 
     // Send version.
-    let send_buf = &mut buf[..version_bytes_len()];
+    let send_buf = &mut buf[..VERSION_BYTES_LEN];
     let handshake = handshake.send_client_version(send_buf)?;
     stream.write_all(send_buf)?;
 
     // Receive version.
-    let recv_buf = &mut buf[..version_bytes_len()];
+    let recv_buf = &mut buf[..VERSION_BYTES_LEN];
     stream.read_exact(recv_buf)?;
     let handshake = handshake.recv_server_version(recv_buf)?;
 
@@ -34,17 +37,17 @@ pub fn client<T: Read + Write>(
     let handshake = handshake.build_client_noise_state_machine()?;
 
     // Send ephemeral key.
-    let send_buf = &mut buf[..ephemeral_key_bytes_len()];
+    let send_buf = &mut buf[..EPHEMERAL_KEY_BYTES_LEN];
     let handshake = handshake.send_client_ephemeral_key(send_buf)?;
     stream.write_all(send_buf)?;
 
     // Receive ephemeral and static keys.
-    let recv_buf = &mut buf[..ephemeral_and_static_key_bytes_len()];
+    let recv_buf = &mut buf[..EPHEMERAL_AND_STATIC_KEY_BYTES_LEN];
     stream.read_exact(recv_buf)?;
     let handshake = handshake.recv_server_ephemeral_and_static_key(recv_buf)?;
 
     // Send static key.
-    let send_buf = &mut buf[..static_key_bytes_len()];
+    let send_buf = &mut buf[..STATIC_KEY_BYTES_LEN];
     let handshake = handshake.send_client_static_key(send_buf)?;
     stream.write_all(send_buf)?;
 
@@ -65,12 +68,12 @@ pub fn server<T: Read + Write>(
     let handshake = Handshake::new_server(version, psk, private_key);
 
     // Receive version.
-    let recv_buf = &mut buf[..version_bytes_len()];
+    let recv_buf = &mut buf[..VERSION_BYTES_LEN];
     stream.read_exact(recv_buf)?;
     let handshake = handshake.recv_client_version(recv_buf)?;
 
     // Send version.
-    let send_buf = &mut buf[..version_bytes_len()];
+    let send_buf = &mut buf[..VERSION_BYTES_LEN];
     let handshake = handshake.send_server_version(send_buf)?;
     stream.write_all(send_buf)?;
 
@@ -78,17 +81,17 @@ pub fn server<T: Read + Write>(
     let handshake = handshake.build_server_noise_state_machine()?;
 
     // Receive ephemeral key.
-    let recv_buf = &mut buf[..ephemeral_key_bytes_len()];
+    let recv_buf = &mut buf[..EPHEMERAL_KEY_BYTES_LEN];
     stream.read_exact(recv_buf)?;
     let handshake = handshake.recv_client_ephemeral_key(recv_buf)?;
 
     // Send ephemeral and static keys.
-    let send_buf = &mut buf[..ephemeral_and_static_key_bytes_len()];
+    let send_buf = &mut buf[..EPHEMERAL_AND_STATIC_KEY_BYTES_LEN];
     let handshake = handshake.send_server_ephemeral_and_static_key(send_buf)?;
     stream.write_all(send_buf)?;
 
     // Receive static key.
-    let recv_buf = &mut buf[..static_key_bytes_len()];
+    let recv_buf = &mut buf[..STATIC_KEY_BYTES_LEN];
     stream.read_exact(recv_buf)?;
     let handshake = handshake.recv_client_static_key(recv_buf)?;
 
